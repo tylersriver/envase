218d37fed0033cbaf2406706bab8bb3232395e4c
Tyler Sriver <tyler.w.sriver@eagles.oc.edu>
2022-03-13 14:47:46 -0500

    Add tests

---
+++ tests/ContainerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Envase\Container;
+use Envase\NotFoundException;
+use Envase\Test\Foo;
+
+it("throws NotFoundException", function () {
+    $c = new Container([]);
+    $c->get('foo');
+})->throws(NotFoundException::class, 'Key not found');
+
+it('finds foo and it equals bar', function() {
+    $c = new Container(['foo' => 'bar']);
+    $val = $c->get('foo');
+    expect($val)->toBe('bar');
+});
+
+it('resolves closure and second find', function() {
+    $c = new Container(['foo' => fn($c) => 'bar']);
+    $val = $c->get('foo');
+    expect($val)->toBe('bar');
+
+    $val = $c->Get('foo');
+    expect($val)->toBe('bar');
+});
+
