0f935b7e2aa11257aeceb9d8ce5aa380af26d955
Tyler Sriver <tyler.w.sriver@eagles.oc.edu>
2023-01-21 12:09:46 -0600

    remove not needed qualifiers

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -6,7 +6,7 @@ use Envase\Test\Foo;
 use Envase\Test\FooDependency;
 
 it("throws NotFoundException", function () {
-    $c = new Container([]);
+    $c = new Container;
     $c->get('foo');
 })->throws(NotFoundException::class, 'Key not found');
 
@@ -26,14 +26,14 @@ it('resolves closure and second find', function() {
 });
 
 it('resolves to Foo class instance', function() {
-    $c = new Container([]);
+    $c = new Container;
     $obj = $c->get(Foo::class);
 
     expect($obj)->toBeInstanceOf(Foo::class);
 });
 
 it('resolves object dependencies', function() {
-    $c = new Container([]);
+    $c = new Container;
     $c->set('fooSet', 'barSet');
     
     /** @var $obj FooDependency */
